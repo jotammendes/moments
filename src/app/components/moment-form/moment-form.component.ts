d5abf31207f16188c077724960ba0e6905b28ad8
João Paulo <jpaulo.mmendes@gmail.com>
2022-05-01 12:28:03 -0300

    feature: implementação de conexão com API e criação de moment

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Moment } from 'src/app/Moment';
 
 @Component({
   selector: 'app-moment-form',
@@ -7,6 +8,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./moment-form.component.css']
 })
 export class MomentFormComponent implements OnInit {
+  @Output() onSubmit = new EventEmitter<Moment>()
   @Input() btnText!: string;
   momentForm!: FormGroup
 
@@ -21,6 +23,11 @@ export class MomentFormComponent implements OnInit {
     })
   }
 
+  onlyFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    this.momentForm.patchValue({image: file});
+  }
+
   get title() {
     return this.momentForm.get('title')!;
   }
@@ -32,7 +39,8 @@ export class MomentFormComponent implements OnInit {
   submit() {
     if(this.momentForm.invalid) return;
 
-    console.log('Enviou o formulário!');
+    console.log('Enviou o formulário!', this.momentForm.value);
+    this.onSubmit.emit(this.momentForm.value);
   }
 
 }
